bb14ecfca18048449e962ec566771c12e6612bbe
Sanjay Kannan <whaatt@gmail.com>
2015-10-12 04:19:18 -0700

    Work.

--- examples/printStanfordYaks.py
+++ examples/printStanfordYaks.py
@@ -4,10 +4,15 @@
 from pyak import *
 
 yakker = Yakker(userID = '93F6BC5EF48244882397787B92DDE98F')
-stanford = Location('37.42636', '-122.171869')
+stanford = Location(37.42636, -122.171869)
 yakker.updateLocation(stanford)
 yaks = yakker.getYaks()
 
 # print out the yaks in ascending order of likes
 for yak in sorted(yaks, key = lambda x: x.time):
   print yak.message.encode('utf-8'), yak.time
+  if (yak.comments > 0):
+    comments = yak.getComments()
+    for comment in comments:
+      print comment.comment, comment.commentID
+    break
--- pyak/location.py
+++ pyak/location.py
@@ -3,11 +3,11 @@
 
 class Location:
   def __init__(self, latitude, longitude, delta = None):
-    self.latitude = latitude
-    self.longitude = longitude
+    self.latitude = str(latitude)
+    self.longitude = str(longitude)
     if delta is None:
       delta = '0.030000'
-    self.delta = delta
+    self.delta = str(delta)
 
   def __str__(self): # human readable representation
     return 'Location(%s, %s)' % (self.latitude, self.longitude)
