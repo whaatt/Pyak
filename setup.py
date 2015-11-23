474670689977332ef51413ed7cd656fc534caadf
Sanjay Kannan <whaatt@gmail.com>
2015-11-23 12:42:56 -0800

    Update setup file.

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@
 from setuptools import setup
 from setuptools import find_packages
 
-setup(name ='pyak', # just run python setup.py install
+setup(name = 'pyak', # just run python setup.py install
       description = 'A Python client for the Yik Yak API',
-      author = 'Jared Smith and Sanjay Kannan [maintainer]',
+      author = 'Sanjay Kannan [current maintainer]',
       version = '0.2.0',
       packages = find_packages(),
       package_data = {},
